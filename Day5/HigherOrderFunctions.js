0194a22ebde2fdfb3d008d6ec493d5ff033385a5
Sowmya B <61962828+Sowmya-bandi@users.noreply.github.com>
2020-07-21 11:07:38 +0530

    Add files via upload

---
+++ Day5/HigherOrderFunctions.js
@@ -0,0 +1,9 @@
+let questionOne = (number) => {
+    let arr=[];
+    for (let i = 0; i < number; i++) {
+         arr.push(i);
+    }
+
+    return arr.filter(val => val % 2 != 0 ).map( val => val ** 3);
+
+}
